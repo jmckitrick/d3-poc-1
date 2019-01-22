6d0c7fe3ab22f9005e57f7b7163d384e7c48f647
Stephen Saucier <stephen.saucier@tstllc.net>
2019-01-22 09:59:27 -0500

    Got a chart to render

--- src/Chart.js
+++ src/Chart.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import './App.css'
-import { XYFrame, ResponsiveXYFrame } from "semiotic"
+import { XYFrame, Mark, ResponsiveXYFrame } from "semiotic"
 
-//var lines = [[[1,2], [3,4], [5,8], [7,16], [9,32], [11,64], [13,128]]];
-
-//var lines={coordinates: [{i: 100, j: 50}, {i: 200, j: 40}, {i: 300, j: 60} ] }
 var lines=[{i: 100, j: 50}, {i: 200, j: 40}, {i: 300, j: 60} ]
 
+const dots = [
+  { user: "Giorgia Meloni", roetweet: 0.02, web: 50, follower: 636661, like:0.03, tweet:787 },
+  { user: "Matteo Salvini", roetweet: 0.02, web: 46, follower: 652043, like:0.06,tweet:4762 },
+  { user: "Matteo Renzi", roetweet: 0.01, web: 60, follower: 3368448,like:0.03,tweet:649 },
+  { user: "Silvio Berlusconi", roetweet: 0.15, web: 44, follower: 25764,like:0.38,tweet:1551 },
+  { user: "Emma Bonino", roetweet: 0.05, web: 40, follower: 157500,like:0.14,tweet:263 },
+  { user: "Pietro Grasso", roetweet: 0.03, web: 28, follower: 61644,like:0.07,tweet:217 },
+  { user: "Casa Pound", roetweet: 0.4, web: 100, follower: 20331,like:0.6,tweet:716 },
+  { user: "Di Maio", roetweet: 0.14, web: 26, follower: 284794,like:0.25,tweet:508 },
+];
 export default class Chart extends Component {
   constructor(props) {
     super(props);
@@ -14,30 +21,31 @@ export default class Chart extends Component {
       //data: eventData,
     }
   }
-/*
-
-          size={ [700, 300] }
-        lines={ {coordinates: [{i: 100, j: 50}, {i: 200, j: 40}, {i: 300, j: 60} ] } }
-
-          xAccessor={ d => d[0] }
-          yAccessor={ d => d[1] }
-          lines={ lines }
-
-        points={[{price: 1.25, size: 15}, {price: 2.25, size: 12}]}
-        pointStyle={{ fill: "blue" }}
-        xAccessor={"price"}
-        yAccessor={"size"}
-*/
 
   render() {
     return(
       <div style={{border: "solid black"}}>
         <span>my chart here</span>
         <XYFrame
-          xAccessor={"i"}
-          yAccessor={"j"}
-          lines={lines}
-          />
+        xExtent={ [0,100] }
+        yExtent={ [0,0.6] }
+        points= {dots}
+        size= {[600,600]}
+        customPointMark={({d}) => <Mark markType="circle" fill='#aa005588' r={d.tweet/80}  />}
+        
+        
+        hoverAnnotation={true}
+        tooltipContent={(d) => (d.user)}
+
+        xAccessor={"web"}
+        yAccessor={this.state.yAccessor}
+        lineIDAccessor="id"
+        margin={{"top":60,"bottom":65,"left":60,"right":20}}
+        axes={[
+          { orient: 'left', tickFormat: d => d },
+          { orient: 'bottom', tickFormat: d => d }
+        ]}
+      />
       </div>
     )
   }
